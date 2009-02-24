b834ff5a3351a2cb83f14cf538a23624f23bf6ca
vsatayamas <vsatayamas@e96ed850-fe87-11dd-a3ef-1f8437fb274a>
2009-02-24 04:43:13 +0000

    capability: save specs

--- main.py
+++ main.py
@@ -27,52 +27,81 @@ import wsgiref.handlers
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
+from google.appengine.ext import db
 import os
 import cgi
 import sys
+from StringIO import StringIO
 from parser import parse
 from drawer import draw
-from StringIO import StringIO
+from utils import statements_to_specs
+import model
 
-def statements_to_specs(statements):
-    d = {'activities': []}
-    for statement in statements:
-        if statement['type'] != 'activity':
-            d[statement['type']] = statement
-        else:
-            d['activities'].append(statement)
-    return d
-            
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.headers['Content-type'] = 'application/xhtml+xml'
-        path = os.path.join(os.path.dirname(__file__), 'index.html');
-        val = {'login_url': users.create_login_url(self.request.uri),
-               'logout_url': users.create_logout_url(self.request.uri).replace("&", "&amp;"),
-               'user': users.get_current_user()}
-        self.response.out.write(template.render(path, val))
+    def common_val(self):
+        specs_list = model.Specs.all()
+        specs_list.filter("author =", users.get_current_user())
+        specs_list.order("-date")
+        val = {'logout_url': users.create_logout_url(self.request.uri).replace("&", "&amp;"),
+               'user': users.get_current_user(),
+               'specs_list': specs_list}
+        return val
 
-    def post(self):
-        self.response.headers['Content-type'] = 'application/xhtml+xml'
-        path = os.path.join(os.path.dirname(__file__), 'index.html');
-        raw_specs = self.request.get('specs')
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            self.response.headers['Content-type'] = 'application/xhtml+xml'
+            path = os.path.join(os.path.dirname(__file__), 'index.html');
+            val = self.common_val()
+            action = self.request.get('action')
+            key = self.request.get('key')
+            if action == "delete":
+                specs = model.Specs.get(key)
+                if specs.author == user:
+                    specs.delete()
+            elif action == "load":
+                specs = model.Specs.get(key)
+                if specs.author == user:
+                    val['specs'] = specs.content
+                    val['title'] = specs.title
+            self.response.out.write(template.render(path, val))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+       
+    def create_svg(self, val, raw_specs):
         statements = parse(raw_specs)
         specs = statements_to_specs(statements)
         sio = StringIO()
         d = draw(specs, sio)
         svg = sio.getvalue()
-        val = {'foo': 12,
-               'svg': svg, 
-               'h': d['maxy'], 
-               'w': d['maxx'],
-               'login_url': users.create_login_url(self.request.uri)}
-        self.response.out.write(template.render(path, val))
+        val['svg'] = svg 
+        val['h'] = d['maxy']
+        val['w'] = d['maxx']
 
-class LoginHandler(webapp.RequestHandler):
-    def get(self):
-        self.redirect(users.create_login_url(self.request.uri))
-        
+    def post(self):
+        user = users.get_current_user()
+        if user:
+            self.response.headers['Content-type'] = 'application/xhtml+xml'
+            path = os.path.join(os.path.dirname(__file__), 'index.html')
+            action = self.request.get('action')
+            val = self.common_val()
+            raw_specs = self.request.get('specs')
+            if action == u"สร้างแผนภูมิ":
+                # create diagram
+                self.create_svg(val, raw_specs)
+            else:
+                # save specs
+                specs = model.Specs()
+                specs.content = raw_specs
+                specs.title = self.request.get('title')
+                specs.author = user
+                specs.put()
+
+            self.response.out.write(template.render(path, val))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)],
                                          debug=True)
